446523298aba6507ed2b0bfaecb0e07cc04fef22
oldmanstreetcoding <oldmanstreetcoding@gmail.com>
2021-07-09 14:46:15 +0700

    last commit before submitting project

--- src/scripts/component/btnFavCreator.js
+++ src/scripts/component/btnFavCreator.js
@@ -1,5 +1,5 @@
 const BtnFavCreator = (type, id) => `<button aria-label="${type}" class="btnSaveFavorite ${type}" id="${id}">
-                                        <img class="tombol" src="./icons/${type}.png" alt="Heart">
+                                        <img class="tombol" src="./icons/${type}.png" alt="Heart" width="44" height="44">
                                     </button>`;
 
 export default BtnFavCreator;
